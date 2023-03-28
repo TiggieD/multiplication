8cc3937706b0d80d9d383dddb4ebd70c1e27a32b
TiggieD <124017914+TiggieD@users.noreply.github.com>
2023-03-28 11:56:47 +0800

    Add files via upload

---
+++ Aguilar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $number = 5; $int; $product;
+
+    for ($int = 0; $int <= 10; $int++){
+        $product = $number * $int;
+        echo "$number x $int = $product";
+        echo '<br>';
+    }
+?>
+</body>
+</html>
